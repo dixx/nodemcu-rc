6e63889b726343fb92373cd86b99919862c34b7e
dixx <jenzdixx@googlemail.com>
2019-10-05 13:39:59 +0200

    Remove unused files, update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include "core.h"
 
 void setup() {
-  // put your setup code here, to run once:
+    serial::init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
