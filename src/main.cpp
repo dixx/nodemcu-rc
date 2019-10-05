c63a972787b8f9dd38d51856f05ada206c693585
dixx <jenzdixx@googlemail.com>
2019-10-05 18:47:04 +0200

    Let there be blinkenlight

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
+#include <stdint.h>
 #include "Arduino.h"
 #include "core.h"
 
+const uint32_t SWITCH = D1;
+const uint32_t FLASH_DURATION = 200; // ms
+
 void setup() {
     serial::init();
+    onboard_led::init();
+    pinMode(SWITCH, INPUT);
 }
 
 void loop() {
+    if (digitalRead(SWITCH) && !onboard_led::isOn()) {
+        onboard_led::on();
+        delay(FLASH_DURATION);
+        onboard_led::off();
+        delay(FLASH_DURATION);
+    }
 }
