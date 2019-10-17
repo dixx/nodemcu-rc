55214e9b11129736d19be8fad64ac3fb45830224
dixx <jenzdixx@googlemail.com>
2019-10-17 12:46:55 +0200

    Try to keep client alive, does not work good

--- src/main.cpp
+++ src/main.cpp
@@ -16,22 +16,36 @@ const uint32_t LED_2 = D6;
 const uint32_t FLASH_DURATION = 200; // ms
 const IPAddress IP(192, 168, 10, 101);
 const IPAddress SUBNET(255, 255, 255, 0);
-const String CLIENT_URL("http://192.168.10.73/rc");
-// Ticker blocker;
-// bool writingAllowed = true; // TODO make class
+const String CLIENT_URL("http://192.168.10.73/");
+Ticker blocker;
+bool keepAlive = true; // TODO make class
 bool inputChanged = false;
 bool clientsConnected = false;
 String data = "";
 
-// void allowWriting() {
-//     writingAllowed = true;
-//     blocker.detach();
-// }
-//
-// void denyWriting() {
-//     writingAllowed = false;
-//     blocker.attach(1.0, allowWriting);
-// }
+void allowSendingKeepAlive() {
+    keepAlive = true;
+    blocker.detach();
+}
+
+void denySendingKeepAlive() {
+    keepAlive = false;
+    blocker.attach(3.0, allowSendingKeepAlive);
+}
+
+void sendKeepAlive() {
+    if (!keepAlive) return;
+
+    WiFiClient client;
+    HTTPClient http;
+    http.begin(client, CLIENT_URL + "keep-alive");
+    Serial.print("keep alive: ");
+    Serial.print(http.GET());
+    Serial.print(", ");
+    Serial.println(http.getString());
+    http.end();
+    denySendingKeepAlive();
+}
 
 void processInput() {
     BUTTON_1.update();
@@ -47,11 +61,12 @@ void checkClients() {
 
 void sendData() {
     WiFiClient client;
+    client.setTimeout(200);
     HTTPClient http;
     http.begin(client, CLIENT_URL);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    data = "";
-    data += "d1=";
+    data = "rc";
+    data += "?d1=";
     data += BUTTON_1.state() ? '1' : '0';
     data += "&d2=";
     data += BUTTON_2.state() ? '1' : '0';
@@ -75,13 +90,12 @@ void setup() {
     SPEED.init();
     pinMode(LED_1, OUTPUT);
     pinMode(LED_2, OUTPUT);
+    WiFi.mode(WIFI_AP);
+    WiFi.setSleepMode(WIFI_NONE_SLEEP);
     WiFi.softAPConfig(IP, IP, SUBNET);
     if (WiFi.softAP(RC_SSID, RC_WLAN_PASSWORD, 8 /* channel */, true /* hidden SSID */, 2 /* max connections */)) {
-        Serial.println("WiFi on.");
-        Serial.print("Soft-AP IP address = ");
+        Serial.print("WiFi on: ");
         Serial.println(WiFi.softAPIP());
-        Serial.print("MAC address = ");
-        Serial.println(WiFi.softAPmacAddress().c_str());
     } else {
         Serial.println("WiFi creation failed.");
     }
@@ -90,5 +104,8 @@ void setup() {
 void loop() {
     checkClients();
     processInput();
-    if (clientsConnected && inputChanged) sendData();
+    if (clientsConnected) {
+        sendKeepAlive();
+        if (inputChanged) sendData();
+    }
 }
