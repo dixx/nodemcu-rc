48eea764c9c7a522bd18de6aea062be068cb288c
dixx <jenzdixx@googlemail.com>
2019-10-21 19:34:47 +0200

    Use network mesh

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-#include <stdint.h>
+#include <cstdint>
+#include <list>
 #include "Arduino.h"
-#include "Ticker.h"
-#include "ESP8266WiFi.h"
-#include "ESP8266HTTPClient.h"
+#include "painlessMesh.h"
 #include "core.h"
 #include "analog_input.h"
 #include "digital_input.h"
@@ -14,72 +13,43 @@ AnalogInput SPEED(A0);
 const uint32_t LED_1 = D5;
 const uint32_t LED_2 = D6;
 const uint32_t FLASH_DURATION = 200; // ms
-const IPAddress IP(192, 168, 10, 101);
-const IPAddress SUBNET(255, 255, 255, 0);
-const String CLIENT_URL("http://192.168.10.73/");
-Ticker blocker;
-bool keepAlive = true; // TODO make class
-bool inputChanged = false;
+const uint32_t MESH_PORT = 5555;
+bool inputAvailable = false;
 bool clientsConnected = false;
-String data = "";
-
-void allowSendingKeepAlive() {
-    keepAlive = true;
-    blocker.detach();
-}
-
-void denySendingKeepAlive() {
-    keepAlive = false;
-    blocker.attach(3.0, allowSendingKeepAlive);
-}
-
-void sendKeepAlive() {
-    if (!keepAlive) return;
-
-    WiFiClient client;
-    HTTPClient http;
-    http.begin(client, CLIENT_URL + "keep-alive");
-    Serial.print("keep alive: ");
-    Serial.print(http.GET());
-    Serial.print(", ");
-    Serial.println(http.getString());
-    http.end();
-    denySendingKeepAlive();
-}
+painlessMesh network;
 
 void processInput() {
     BUTTON_1.update();
     BUTTON_2.update();
     SPEED.update();
-    inputChanged = BUTTON_1.hasChanged() || BUTTON_2.hasChanged();
+    inputAvailable = BUTTON_1.state() || BUTTON_2.state();
 }
 
 void checkClients() {
-    clientsConnected = (WiFi.softAPgetStationNum() > 0);
+    std::list<uint32_t> nodes = network.getNodeList();
+    clientsConnected = (nodes.size() > 0);
     digitalWrite(LED_2, clientsConnected ? HIGH : LOW);
 }
 
 void sendData() {
-    WiFiClient client;
-    client.setTimeout(200);
-    HTTPClient http;
-    http.begin(client, CLIENT_URL);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    data = "rc";
-    data += "?d1=";
-    data += BUTTON_1.state() ? '1' : '0';
-    data += "&d2=";
-    data += BUTTON_2.state() ? '1' : '0';
-    data += "&a=";
-    data += SPEED.value();
-    Serial.print("Send: ");
-    Serial.print(CLIENT_URL);
-    Serial.println(data);
-    Serial.print("HTTP status code: ");
-    Serial.println(http.POST(data));
-    Serial.print("HTTP returned: ");
-    Serial.println(http.getString());
-    http.end();
+    digitalWrite(LED_1, HIGH);
+    String message = "";
+    message += "d1=";
+    message += BUTTON_1.state() ? "1" : "0";
+    message += ":d2=";
+    message += BUTTON_2.state() ? "1" : "0";
+    message += ":a=";
+    message += SPEED.value();
+    network.sendBroadcast(message);
+    digitalWrite(LED_1, LOW);
+}
+
+void receivedCallback(uint32_t from, String &msg) {
+    Serial.printf("startHere: Received from %d msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(bool adopt) {
+    Serial.printf("startHere: New Connection, adopt=%d\n", adopt);
 }
 
 void setup() {
@@ -90,22 +60,18 @@ void setup() {
     SPEED.init();
     pinMode(LED_1, OUTPUT);
     pinMode(LED_2, OUTPUT);
-    WiFi.mode(WIFI_AP);
-    WiFi.setSleepMode(WIFI_NONE_SLEEP);
-    WiFi.softAPConfig(IP, IP, SUBNET);
-    if (WiFi.softAP(RC_SSID, RC_WLAN_PASSWORD, 8 /* channel */, true /* hidden SSID */, 2 /* max connections */)) {
-        Serial.print("WiFi on: ");
-        Serial.println(WiFi.softAPIP());
-    } else {
-        Serial.println("WiFi creation failed.");
-    }
+    network.setDebugMsgTypes(ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE); // all types on
+    //network.setDebugMsgTypes(ERROR | STARTUP);  // set before init() so that you can see startup messages
+    network.init(RC_SSID, RC_WLAN_PASSWORD, MESH_PORT);
+    network.onReceive(&receivedCallback);
+    network.onNewConnection(&newConnectionCallback);
 }
 
 void loop() {
+    network.update();
     checkClients();
     processInput();
     if (clientsConnected) {
-        sendKeepAlive();
-        if (inputChanged) sendData();
+        if (inputAvailable) sendData();
     }
 }
